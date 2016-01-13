0af2c10c364c5cda510d39408d9d0ff204441c1c
Hideki Abe <hideki.abe.0630@gmail.com>
2016-01-13 15:51:33 +0900

    Correspond mt vars(only dynamic)

--- plugins/SetRandomNumVar/php/function.mtsetrandomnumvar.php
+++ plugins/SetRandomNumVar/php/function.mtsetrandomnumvar.php
@@ -1,7 +1,9 @@
 <?php
 function smarty_function_mtsetrandomnumvar($args, &$ctx) {
     $range = $args['range'];
+    embed_vars_val($range, $ctx);
     preg_match("/^\"(\d+)\":\"(\d+)\"$/", $range, $matches);
+
     $min = $matches[1];
     $max = $matches[2];
 
@@ -9,4 +11,10 @@ function smarty_function_mtsetrandomnumvar($args, &$ctx) {
     $vars[$args['name']] = rand($min, $max);
     $ctx->__stash['vars'] =& $vars;
     return '';
+}
+
+function embed_vars_val(&$range, &$ctx) {
+    $range = preg_replace_callback("/Array\.(.*?)(\:|$)/", function ($m) use ($ctx) {
+        return '"' . $ctx->__stash['vars'][$m[1]] . '"' . $m[2];
+    }, $range);
 }
